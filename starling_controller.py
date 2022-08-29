8c900adbd7ff8088f52882e2b429810568d56cc6
ken <kenvu@tutanota.com>
2022-08-29 14:07:39 +0100

    Implemented api _call function. Implemented get_accounts tempoarily.

--- starling_controller.py
+++ starling_controller.py
@@ -1,9 +1,34 @@
+import requests as r
+
+
 class StarlingCtrl:
-    __request_string = "Authorization: Bearer "
+    _base_endpoint = "https://api.starlingbank.com/api/v2"
+    _header = ""
 
-    def __init__(self) -> None:
-        self.authorised = None
-        self.session_token = None
+    def __init__(self, authorised, session_token) -> None:
+        self.authorised = authorised
+        self.session_token = session_token
+        self._header = {'Authorization': 'Bearer ' + self.session_token}
 
-    def pat(self, pat):
-        self.session_token = pat
+    def _call(self, method, endpoint, body=None):
+        try:
+            response = None
+            if method == 'GET':
+                response = r.get(self._base_endpoint + endpoint, headers=self._header)
+            elif method == 'POST':
+                response = r.post(self._base_endpoint + endpoint, headers=self._header, data=body)
+            response.raise_for_status()
+            return response
+        except r.exceptions.HTTPError as errh:
+            return "An Http Error occurred:" + repr(errh)
+        except r.exceptions.ConnectionError as errc:
+            return "An Error Connecting to the API occurred:" + repr(errc)
+        except r.exceptions.Timeout as errt:
+            return "A Timeout Error occurred:" + repr(errt)
+        except r.exceptions.RequestException as err:
+            return "An Unknown Error occurred" + repr(err)
+
+    def get_accounts(self):
+        res = self._call('GET', '/accounts')
+        print(res.text)
+        return res
